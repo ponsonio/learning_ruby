baf377cbbe8a69c4e2a9cf182df3c2b4d3bc1293
Jorge Cabrera <jcabrera@Jorges-MacBook-Pro.local>
2019-07-24 14:22:13 -0500

    Update merge_sort.rb

---
+++ algos/merge_sort.rb
@@ -0,0 +1,12 @@
+def merge_sort (list)
+
+end
+
+def merge(left, right)
+    if left.empty?
+        right
+    elsif right.empty?
+        left
+    elsif left.first < right.first
+        [left.first] +     
+end
